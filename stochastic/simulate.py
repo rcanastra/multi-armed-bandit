ebc20e46fab517cd5ac7ded3f87d9798c8e6d7ad
Ryoma Canastra <ryoma.canastra@gmail.com>
2025-10-31 12:35:58 -0700

    some more uncommited changes

--- stochastic/simulate.py
+++ stochastic/simulate.py
@@ -5,39 +5,40 @@ from strategy import *
 import numpy as np
 from typing import Callable
 
+# Arms can be made non-independent by implementing StochasticBandit
+# in some appropriate way. And arm pulls can be made non stationary
+# similarly. "Stochastic" is not really the right term, given this.
 def simulate(
     strats: List[StochasticBanditStrategy],
     bandit: StochasticBandit,
     num_rounds: int,
-    num_simulations: int
-) -> List[List[float]]:
-    results = [[] for _ in range(len(strats))]
-    for i in range(num_simulations):
-        for j in range(num_rounds):
-            bandit.fix_probabilistic_state()
-            for k, strat in enumerate(strats):
-                arm = strat.choose_arm()
-                result = bandit.pull_arm(arm)
-                results[k].append(result)
-                strat.record_result(arm, result)
+) -> np.ndarray:
+    results = np.empty((len(strats), num_rounds))
+    for i in range(num_rounds):
+        bandit.fix_probabilistic_state()
+        for j, strat in enumerate(strats):
+            arm = strat.choose_arm()
+            result = bandit.pull_arm(arm)
+            results[j, i] = result
+            strat.record_result(arm, result)
                 
     return results
 
-parser = argparse.ArgumentParser(description='Stochastic multi-armed bandit')
-# ignore these, too hard to deal with
-# parser.add_argument('bandit', type=str)
-# parser.add_argument('strats', type=str)
-parser.add_argument('--num-rounds', '-r', type=int)
-parser.add_argument('--num-simulations', '-s', type=int)
+# parser = argparse.ArgumentParser(description='Stochastic multi-armed bandit')
+# # ignore these, too hard to deal with
+# # parser.add_argument('bandit', type=str)
+# # parser.add_argument('strats', type=str)
+# parser.add_argument('--num-rounds', '-r', type=int)
 
-args = parser.parse_args()
+# args = parser.parse_args()
 
-# bandit = args.bandit
-# strats = args.strats
+# # bandit = args.bandit
+# # strats = args.strats
 
-num_rounds = args.num_rounds
-num_simulations = args.num_simulations
+# num_rounds = args.num_rounds
+
+num_rounds = 10
 
 # print(simulate([MyStrategy()], SimpleBandit([0.1, 0.9]), 1000, 10))
-results = simulate([ThompsonSamplingBeta(2)], BernoulliBandit([0.1, 0.9]), num_rounds, num_simulations)
+results = simulate([ThompsonSamplingBeta(2)], BernoulliBandit([0.1, 0.9]), num_rounds)
 print(np.sum(results))
